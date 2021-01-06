ff0a16fe982103bcb59a0eb5fbff792b2a9eb07b
SigmaSix6 <SigmaSix6@users.noreply.github.com>
2021-01-06 16:53:04 +0700

    Create validation and tooltip on Strength Modifier

--- src/Components/ModifierInput.js
+++ src/Components/ModifierInput.js
@@ -5,8 +5,19 @@ const ModifierInput = (props) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
       <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'column', padding: 20 }}>
-        <input className='form-control cursor-pointer' type='number' value={props.strMod} min={-5} max={10} style={{ fontSize: 40, textAlign: 'center' }} onChange={(e) => props.changeStrMod(e.target.value)} />
-        <h4 style={{ textAlign: 'center' }}>Strength Modifier</h4>
+        <div className='tool-tip'>
+          <input className='form-control cursor-pointer' type='number' value={props.strMod} min={-5} max={10} style={{ fontSize: 40, textAlign: 'center' }} onChange={(e) => 
+            {if (e.target.value > 10){
+              e.target.value = 10;
+            }
+            else if (e.target.value < -5){
+              e.target.value = -5
+            }
+            props.changeStrMod(e.target.value)}
+            } />
+            <span className='tool-tiptext'>Only Values between -5 and 10 will be accepted</span>
+          </div>
+        <h4 style={{ textAlign: 'center' }}>Strength Modifier </h4>        
       </div>
     </div>
   )
